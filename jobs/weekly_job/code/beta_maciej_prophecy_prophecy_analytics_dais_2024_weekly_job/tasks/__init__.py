b37c04d19ff1db609c4ec4735d2b51b1319c500c
several27@icloud.com <several27@icloud.com>
2024-09-03 15:45:42 +0000

    Refactors weekly_job tasks and adds dbt_utils module for dbt command execution

--- jobs/weekly_job/code/beta_maciej_prophecy_prophecy_analytics_dais_2024_weekly_job/tasks/__init__.py
+++ jobs/weekly_job/code/beta_maciej_prophecy_prophecy_analytics_dais_2024_weekly_job/tasks/__init__.py
@@ -1,4 +1,4 @@
-from .extract_to_tab import *
 from .analytics import *
-from .wait_for_data import *
+from .extract_to_tab import *
 from .notify_users import *
+from .wait_for_data import *
--- models/silver/google_ads/sv_go_account_report.sql
+++ models/silver/google_ads/sv_go_account_report.sql
@@ -1,3 +1,90 @@
+WITH br_go_account_history AS (
+
+  SELECT * 
+  
+  FROM {{ ref('br_go_account_history')}}
+
+),
+
+br_go_account_stats AS (
+
+  SELECT * 
+  
+  FROM {{ ref('br_go_account_stats')}}
+
+),
+
+by_account_id_source_relation AS (
+
+  SELECT 
+    br_go_account_stats.date_day AS date_day,
+    br_go_account_stats.ad_network_type AS ad_network_type,
+    br_go_account_stats.device AS device,
+    br_go_account_stats.clicks AS clicks,
+    br_go_account_stats.spend AS spend,
+    br_go_account_stats.impressions AS impressions,
+    br_go_account_history.currency_code AS currency_code,
+    br_go_account_history.updated_at AS updated_at,
+    br_go_account_history.account_id AS account_id,
+    br_go_account_history.source_relation AS source_relation,
+    br_go_account_history.account_name AS account_name,
+    br_go_account_history.auto_tagging_enabled AS auto_tagging_enabled,
+    br_go_account_history.time_zone AS time_zone,
+    br_go_account_history.is_most_recent_record AS is_most_recent_record
+  
+  FROM br_go_account_history
+  INNER JOIN br_go_account_stats
+     ON br_go_account_stats.account_id = br_go_account_history.account_id
+    AND br_go_account_stats.source_relation = br_go_account_history.source_relation
+
+),
+
+account_performance_summary AS (
+
+  {#Summarizes account performance metrics, including clicks, impressions, and spending.#}
+  SELECT 
+    account_id,
+    SUM(clicks) AS TOTAL_CLICKS,
+    SUM(impressions) AS TOTAL_IMPRESSIONS,
+    SUM(spend) AS TOTAL_SPEND,
+    any_value(date_day) AS date_day,
+    any_value(ad_network_type) AS ad_network_type,
+    any_value(source_relation) AS source_relation,
+    any_value(currency_code) AS currency_code
+  
+  FROM by_account_id_source_relation
+  
+  GROUP BY account_id
+
+),
+
+account_performance_summary_1 AS (
+
+  {#Summarizes key performance metrics for advertising accounts.#}
+  SELECT 
+    account_id AS account_id,
+    TOTAL_CLICKS AS TOTAL_CLICKS,
+    TOTAL_IMPRESSIONS AS TOTAL_IMPRESSIONS,
+    TOTAL_SPEND AS TOTAL_SPEND,
+    date_day AS date_day,
+    ad_network_type AS ad_network_type,
+    source_relation AS source_relation,
+    currency_code AS currency_code
+  
+  FROM account_performance_summary
+
+),
+
+limit_100 AS (
+
+  SELECT * 
+  
+  FROM account_performance_summary_1
+  
+  LIMIT 100
+
+)
+
 SELECT *
 
 FROM limit_100
